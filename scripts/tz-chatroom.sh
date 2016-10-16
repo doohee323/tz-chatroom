ebdf2f5ca254ad4379b431cd95efd19ad26860ed
Hong, Dewey <doohee323@gmail.com>
2016-10-15 21:32:24 -0700

    .

--- scripts/tz-chatroom.sh
+++ scripts/tz-chatroom.sh
@@ -6,37 +6,38 @@ export SENV=$1
 
 export USER=vagrant  # for vagrant
 export PROJ_NAME=tz-chatroom
-export PROJ_DIR=/vagrant
+export PROJ_DIR=/tz-chatroom
 export NODE_ENV=development
-export SRC_DIR=/vagrant/resources  # for vagrant
 
 if [ "$SENV" = "aws" ]; then
-	USER=ubuntu
-	SRC_DIR=/home/ubuntu/tz-chatroom/resources
+	export USER=ubuntu
+	cd /
+	sudo git clone https://github.com/doohee323/tz-chatroom.git
+	sudo chown -Rf ubuntu:ubuntu /tz-chatroom
 fi
 
+export SRC_DIR=/tz-chatroom/resources
 export HOME_DIR=/home/$USER
 
 sudo sh -c "echo '' >> $HOME_DIR/.bashrc"
-sudo sh -c "echo 'export PATH=$PATH:.' >> $HOME_DIR/.bashrc"
+sudo sh -c "echo 'export PATH=$PATH:.:~/play-2.1.3' >> $HOME_DIR/.bashrc"
 sudo sh -c "echo 'export HOME_DIR='$HOME_DIR >> $HOME_DIR/.bashrc"
+sudo sh -c "echo 'export PROJ_DIR='$PROJ_DIR >> $HOME_DIR/.bashrc"
 sudo sh -c "echo 'export SRC_DIR='$SRC_DIR >> $HOME_DIR/.bashrc"
-sudo sh -c "echo 'export NODE_ENV=development >> $HOME_DIR/.bashrc"
-sudo sh -c "echo 'export PORT=3000 >> $HOME_DIR/.bashrc"
 source $HOME_DIR/.bashrc
 
 sudo sh -c "echo '' >> /etc/hosts"
-sudo sh -c "echo '127.0.0.1  www.topzone.biz' >> /etc/hosts"
-sudo sh -c "echo '127.0.0.1  admin.topzone.biz' >> /etc/hosts"
-
-sudo apt-get install software-properties-common -y
-sudo add-apt-repository ppa:ondrej/php -y
+sudo sh -c "echo '127.0.0.1  chatroom.topzone.biz' >> /etc/hosts"
 
 sudo apt-get update
 
+sudo apt-get install openjdk-7-jdk curl -y
 sudo apt-get install npm -y
+sudo apt-get install nodejs-legacy
 sudo apt-get install git -y
+sudo apt-get install unzip
 sudo npm install bower -g
+sudo npm install -g grunt-cli 
 
 sudo locale-gen UTF-8
 
@@ -45,41 +46,49 @@ sudo apt-get install nginx -y --force-yes
 
 sudo cp -rf $SRC_DIR/nginx/nginx.conf /etc/nginx/nginx.conf
 # tz-chatroom
-sudo rm -rf /etc/nginx/sites-available/default
-sudo cp -Rf $SRC_DIR/nginx/default /etc/nginx/sites-available/default
-sudo ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
-# tz-chatroom-admin
-sudo rm -rf /etc/nginx/sites-available/admin
-sudo cp -Rf $SRC_DIR/nginx/admin /etc/nginx/sites-available/admin
-sudo ln -s /etc/nginx/sites-available/admin /etc/nginx/sites-enabled/admin
-# adminlte
-sudo rm -rf /etc/nginx/sites-available/adminlte
-sudo cp -Rf $SRC_DIR/nginx/adminlte /etc/nginx/sites-available/adminlte
-sudo ln -s /etc/nginx/sites-available/adminlte /etc/nginx/sites-enabled/adminlte
+sudo rm -rf /etc/nginx/sites-available/tz-chatroom.conf
+sudo rm -rf /etc/nginx/sites-enabled/tz-chatroom.conf
+sudo cp -Rf $SRC_DIR/nginx/tz-chatroom.conf /etc/nginx/sites-available/tz-chatroom.conf
+sudo ln -s /etc/nginx/sites-available/tz-chatroom.conf /etc/nginx/sites-enabled/tz-chatroom.conf
+sudo service nginx restart
 
 if [ "$SENV" = "aws" ]; then
-	sudo sed -i "s/\/vagrant\/web_apps/\/home\/ubuntu\/tz-chatroom\/web_apps/g" /etc/nginx/sites-available/default
-	sudo sed -i "s/\/vagrant\/web_apps/\/home\/ubuntu\/tz-chatroom-admin\/web_apps/g" /etc/nginx/sites-available/admin
+	sudo sed -i "s/\/tz-chatroom\/web_apps/\/home\/ubuntu\/tz-chatroom\/web_apps/g" /etc/nginx/sites-available/default
+	sudo sed -i "s/\/tz-chatroom\/web_apps/\/home\/ubuntu\/tz-chatroom-admin\/web_apps/g" /etc/nginx/sites-available/admin
 fi
 
 # curl http://127.0.0.1:80
 sudo service nginx stop
 
 ### [install redis] ############################################################################################################
+cd $HOME_DIR
+wget http://download.redis.io/redis-stable.tar.gz
+tar xvzf redis-stable.tar.gz
+cd redis-stable
+make
+sudo cp src/redis-server /usr/local/bin/
+sudo cp src/redis-cli /usr/local/bin/
+redis-server &
 
-### [install php] ############################################################################################################
-sudo apt-get install php7.0-fpm -y
-sudo apt-get install php7.0-redis -y
-sudo apt-get install php-xdebug -y
-sudo apt-get install php-curl -y
-sudo service php7.0-fpm stop 
-sudo cp -rf $SRC_DIR/php70/php.ini /etc/php/7.0/fpm/php.ini
+### [install play!] ############################################################################################################
+cd $HOME_DIR
+wget https://downloads.typesafe.com/play/2.1.3/play-2.1.3.zip
+unzip play-2.1.3.zip
+sudo chown -Rf vagrant:vagrant play-2.1.3
+if [ "$SENV" = "aws" ]; then
+	sudo chown -Rf ubuntu:ubuntu play-2.1.3
+fi
+cd $PROJ_DIR
+bash build.sh compile
 
-sudo cp -rf $SRC_DIR/php70/admin.conf /etc/php/7.0/fpm/pool.d/admin.conf
-sudo cp -rf $SRC_DIR/php70/adminlte.conf /etc/php/7.0/fpm/pool.d/adminlte.conf
-
-### [install socket.io] ############################################################################################################
-
+### [make web env] ############################################################################################################
+cd $PROJ_DIR/web
+npm install
+bower install
+if [ "$SENV" = "aws" ]; then
+	sudo chown -Rf ubuntu:ubuntu play-2.1.3
+fi
+grunt build --force
 
 ### [open firewalls] ############################################################################################################
 ufw allow "Nginx Full"
@@ -87,24 +96,15 @@ sudo iptables -I INPUT -p tcp --dport 21 -j ACCEPT
 sudo iptables -I INPUT -p tcp --dport 22 -j ACCEPT
 sudo iptables -I INPUT -p tcp --dport 80 -j ACCEPT
 sudo iptables -I INPUT -p tcp --dport 443 -j ACCEPT
-sudo iptables -I INPUT -p tcp --dport 3002 -j ACCEPT
-sudo iptables -I INPUT -p tcp --dport 3306 -j ACCEPT
 sudo iptables -I INPUT -p tcp --dport 9000 -j ACCEPT
-sudo iptables -I INPUT -p tcp --dport 9005 -j ACCEPT
-sudo iptables -I INPUT -p tcp --dport 8090 -j ACCEPT
 sudo service iptables save
 sudo service iptables restart
 
 ### [start services] ############################################################################################################
-sudo mkdir -p /tmp/uploads
-sudo ln -s /tmp/uploads /vagrant/web_apps/uploads
-sudo chown -Rf vagrant:vagrant /tmp/uploads
-sudo chmod -Rf 777 /tmp/uploads
-
-#redis -h localhost -P 3306 -u root -p
+cd $PROJ_DIR
 sudo /etc/init.d/redis restart  
-sudo service php7.0-fpm restart
 sudo service nginx start
+bash build.sh start
 
 #curl http://192.168.82.120
 
